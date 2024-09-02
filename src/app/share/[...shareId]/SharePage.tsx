61a5bc2a41fbe51b9520ece80999cab752cf4995
Tadd Giles <tadd@taddgiles.com>
2024-09-02 14:25:42 -0600

    update to support embedded iframe

--- src/app/share/[...shareId]/SharePage.tsx
+++ src/app/share/[...shareId]/SharePage.tsx
@@ -6,9 +6,27 @@ import Header from './Header';
 import Footer from './Footer';
 import styles from './SharePage.module.css';
 import { WebsiteProvider } from 'app/(main)/websites/[websiteId]/WebsiteProvider';
+import { useEffect } from 'react';
+import { useTheme } from 'components/hooks';
 
 export default function SharePage({ shareId }) {
   const { shareToken, isLoading } = useShareToken(shareId);
+  const { saveTheme } = useTheme();
+
+  useEffect(() => {
+    if (window.self !== window.top) {
+      document.documentElement.setAttribute('data-iframe', '');
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', ({ matches }) => {
+          if (matches) {
+            saveTheme('dark');
+          } else {
+            saveTheme('light');
+          }
+        });
+    }
+  }, []);
 
   if (isLoading || !shareToken) {
     return null;
